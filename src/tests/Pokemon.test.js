6b5813d244bd323a3afa3fc877137fc4bf5dec65
Zirlanefiuza <zirlanebatistafiuza2@gmail.com>
2023-03-08 00:43:53 -0300

    requisito 6 e 7 concluido

--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -1,1 +1,48 @@
-test('', () => {});
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import renderWithRouter from '../renderWithRouter';
+import pokemonList from '../data';
+import App from '../App';
+
+const firstPokemon = pokemonList[0];
+const details = 'More details';
+
+test('Teste se é renderizado um card com as informações de determinado Pokémon', () => {
+  renderWithRouter(<App />);
+  const pokemonName = screen.getByTestId('pokemon-name');
+  expect(pokemonName).toHaveTextContent(firstPokemon.name);
+  const typePokemon = screen.getByTestId('pokemon-type');
+  expect(typePokemon).toHaveTextContent(firstPokemon.type);
+  const pokemonWeight = screen.getByTestId('pokemon-weight');
+  const averageText = 'Average weight: '
+    + `${firstPokemon.averageWeight.value} ${firstPokemon.averageWeight.measurementUnit}`;
+  expect(pokemonWeight).toHaveTextContent(averageText);
+
+  const pokemonImgAlt = `${firstPokemon.name} sprite`;
+  const image = screen.getByAltText(pokemonImgAlt);
+  expect(image).toBeVisible();
+  expect(image.src).toBe(firstPokemon.image);
+});
+test('Teste se o card do Pokémon indicado na Pokédex contém um link de navegação para exibir detalhes deste Pokémon', () => {
+  renderWithRouter(<App />);
+  const pokemonLink = screen.getByRole('link', { name: details });
+  expect(pokemonLink.href).toContain(`/pokemon/${firstPokemon.id}`);
+});
+test('Teste se ao clicar no link de navegação do Pokémon, é feito o redirecionamento da aplicação para a página de detalhes de Pokémon', () => {
+  const { history } = renderWithRouter(<App />);
+  const linkMoreDetails = screen.getByRole('link', { name: details });
+  userEvent.click(linkMoreDetails);
+  const textPokemon = screen.getByRole('heading', { name: `${firstPokemon.name} Details` });
+  expect(textPokemon).toBeVisible();
+  expect(history.location.pathname).toBe(`/pokemon/${firstPokemon.id}`);
+});
+test('Teste se existe um ícone de estrela nos Pokémon favoritados', () => {
+  renderWithRouter(<App />);
+  const linkMoreDetails = screen.getByRole('link', { name: details });
+  userEvent.click(linkMoreDetails);
+  const checkBox = screen.getByRole('checkbox');
+  userEvent.click(checkBox);
+  const star = screen.getByRole('img', { name: `${firstPokemon.name} is marked as favorite` });
+  expect(star).toBeVisible();
+  expect(star.src).toContain('/star-icon.svg');
+});
--- src/tests/PokemonDetails.test.js
+++ src/tests/PokemonDetails.test.js
@@ -1,1 +1,48 @@
-test('', () => {});
+import { screen } from '@testing-library/react';
+import { act } from 'react-dom/test-utils';
+import userEvent from '@testing-library/user-event';
+import App from '../App';
+import renderWithRouter from '../renderWithRouter';
+import data from '../data';
+
+const path = '/pokemon/25';
+
+test('Teste se a página contém um elemento com o texto "Pikachu Details"', () => {
+  const { history } = renderWithRouter(<App />);
+  expect(history.location.pathname).toBe('/');
+  const linkPokemon = screen.getByRole('link', { name: 'More details' });
+  expect(linkPokemon.href.includes(path)).toBe(true);
+  act(() => {
+    history.push(path);
+  });
+  expect(history.location.pathname).toBe(path);
+  expect(screen.getByRole('heading', { name: 'Pikachu Details', level: 2 })).toBeInTheDocument();
+  expect(linkPokemon).not.toBeInTheDocument();
+  expect(screen.getByRole('heading', { level: 2, name: 'Summary' }));
+  expect(screen.getByText(/electricity/i)).toBeInTheDocument();
+});
+
+test('Renderiza as informações dos mapas corretamente', () => {
+  const { foundAt } = data[0];
+  const { history } = renderWithRouter(<App />);
+  act(() => {
+    history.push(path);
+  });
+  expect(screen.getByRole('heading', { level: 2, name: 'Game Locations of Pikachu' })).toBeInTheDocument();
+  foundAt.forEach(({ location, map }, index) => {
+    expect(screen.getByText(location)).toBeInTheDocument();
+    const mapImage = screen.getAllByRole('img', { name: 'Pikachu location' });
+    expect(mapImage[index]).toBeInTheDocument();
+    expect(mapImage[index]).toHaveProperty('src', map);
+  });
+});
+
+test('Teste se é possível favoritar o pokemon pela página de detalhes', () => {
+  const { history } = renderWithRouter(<App />);
+  act(() => {
+    history.push(path);
+  });
+  const favorite = screen.getByLabelText('Pokémon favoritado?');
+  userEvent.click(favorite);
+  expect(screen.getByRole('img', { name: 'Pikachu is marked as favorite' })).toBeInTheDocument();
+});
